367b6e7aaa5879e10d5aec5ce000bec2b3c8ad88
Cesar Morales Garduño <shell.one.core@gmail.com>
2020-11-29 16:29:52 -0600

    Se agregaron más ejemplos de operadores condicionales

--- Ejercicios/06_operadores.dart
+++ Ejercicios/06_operadores.dart
@@ -15,5 +15,37 @@ main() {
   // print(resp);
 
   int d = b ?? a ?? 100; // Si b es nulo, d tendrá el valor de a
-  print(d);
+  // print(d);
+
+  // Operadores relacionales
+  /*
+    > Mayor que
+    < Menor que
+    >= Mayor o igual que
+    <= Menor o igual que
+
+    == igual que
+    != distinto que
+  */
+
+  String persona1 = 'Fernando';
+  String persona2 = 'Alberto';
+
+  // print(persona1 == persona2);
+  // print(persona1 != persona2);
+
+  int x = 20;
+  int y = 30;
+
+  // print(x > y);
+  // print(x >= y);
+  // print(x < y);
+  // print(x <= y);
+
+  // Operador de tipo
+  int i = 10;
+  String j = "10";
+
+  print(i is int);
+  print(j is! int);
 }
