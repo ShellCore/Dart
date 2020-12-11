6d60752355da06bf8652dc651bdd2390df81d985
Cesar Morales Garduño <shell.one.core@gmail.com>
2020-12-11 15:33:52 -0600

    Ejemplo de ciclo for in

---
+++ 02_control/04_forin.dart
@@ -0,0 +1,11 @@
+main() {
+  List<String> superheroes = ['Batman', 'Superman', 'Mujer Maravilla'];
+
+  // for (int i = 0; i < superheroes.length; i++) {
+  //   print(superheroes[i]);
+  // }
+
+  for (String heroe in superheroes) {
+    print(heroe);
+  }
+}
