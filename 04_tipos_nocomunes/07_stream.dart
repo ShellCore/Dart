72407e17b27a3c4f34f73ef6a7324909686c2591
Cesar Morales Garduño <shell.one.core@gmail.com>
2020-12-12 19:41:20 -0600

    Ejemplos de streams

---
+++ 04_tipos_nocomunes/07_stream.dart
@@ -0,0 +1,21 @@
+import 'dart:async';
+
+main() {
+  final streamController = new StreamController<String>();
+
+  streamController.stream.listen((data) => print('Despegando! $data'),
+      onError: (err) => print('Error: $err'),
+      onDone: () => print('¡Misión completa!'),
+      cancelOnError: false);
+
+  streamController.sink.add('Apollo 11');
+  streamController.sink.add('Apollo 12');
+  streamController.sink.add('Apollo 13');
+  streamController.sink.addError('Houston, tenemos un problema!');
+  streamController.sink.add('Apollo 14');
+  streamController.sink.add('Apollo 15');
+
+  streamController.sink.close();
+
+  print('Fin del main');
+}
---
+++ 04_tipos_nocomunes/07_stream_multiple.dart
@@ -0,0 +1,26 @@
+import 'dart:async';
+
+main() {
+  final streamController = new StreamController<String>.broadcast();
+
+  streamController.stream.listen((data) => print('Despegando Stream 1! $data'),
+      onError: (err) => print('Error: $err'),
+      onDone: () => print('¡Misión completa!'),
+      cancelOnError: false);
+
+  streamController.stream.listen((data) => print('Despegando Stream 2! $data'),
+      onError: (err) => print('Error (2): $err'),
+      onDone: () => print('¡Misión completa en stream 2!'),
+      cancelOnError: false);
+
+  streamController.sink.add('Apollo 11');
+  streamController.sink.add('Apollo 12');
+  streamController.sink.add('Apollo 13');
+  streamController.sink.addError('Houston, tenemos un problema!');
+  streamController.sink.add('Apollo 14');
+  streamController.sink.add('Apollo 15');
+
+  streamController.sink.close();
+
+  print('Fin del main');
+}
