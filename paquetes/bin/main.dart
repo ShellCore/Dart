39ad221652952facf7d8763b733bed7d2ee32f49
Cesar Morales Garduño <shell.one.core@gmail.com>
2020-12-28 17:44:37 -0600

    Se agregó librería http y ejemplo de consumo del servicio

--- paquetes/bin/main.dart
+++ paquetes/bin/main.dart
@@ -1,5 +1,10 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'package:paquetes/paquetes.dart' as paquetes;
 
 void main(List<String> arguments) {
-  print('Hello world: ${paquetes.calculate()}!');
+  final url = 'https://reqres.in/api/users';
+  http.get(url).then((response) {
+    print(response);
+  });
 }
