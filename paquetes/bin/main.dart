28e5c48b4d0a738925cdaf82a1d41f6f6e1a0259
Cesar Morales Garduño <shell.one.core@gmail.com>
2020-12-28 17:49:57 -0600

    Consumo de datos del response

--- paquetes/bin/main.dart
+++ paquetes/bin/main.dart
@@ -5,6 +5,10 @@ import 'package:paquetes/paquetes.dart' as paquetes;
 void main(List<String> arguments) {
   final url = 'https://reqres.in/api/users';
   http.get(url).then((response) {
-    print(response);
+    final body = jsonDecode(response.body);
+    print(body);
+    print("page: ${body['page']}");
+    print("per_page: ${body['per_page']}");
+    print("id del tercer elemento: ${body['data'][2]['id']}");
   });
 }
